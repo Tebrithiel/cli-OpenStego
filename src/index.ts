fce947fcc3a756f23cb8e7545a48e2b8784da9e2
Tebrithiel <20450667+Tebrithiel@users.noreply.github.com>
2024-05-11 19:43:13 +1000

    Updated main index file to test current classes and functions
    We want to know that the basic functionality of the script works so index has been updated to include the useage of current functions
    
    - Updated src/index.ts to use CLIInterface class
    - Updated src/index.ts to use functions found in CLIInterface class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('This is the inital commit with Biome, typescipt, and corepack')
+import { CLIInterface } from './components'
+
+const testCredentials = {
+	'1Password': 'qergoihqergQERGQAErgoih230987',
+	'1Password Secret': 'awefg-saerg98234AGF',
+	'Another secret encoded in stego': 'Secret_text',
+}
+
+async function main() {
+	const terminal = new CLIInterface()
+
+	const test = await terminal.addCredential()
+
+	console.log(test)
+
+	const edited = await terminal.editCredentials(testCredentials)
+
+	console.log(edited)
+}
+
+main()
