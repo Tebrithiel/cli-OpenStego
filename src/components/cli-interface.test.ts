b0baebe0fa2a679a49f805111743e6f621fd687b
Tebrithiel <20450667+Tebrithiel@users.noreply.github.com>
2024-05-12 22:12:10 +1000

    Added getStegoPassword property
    Some steganography files are pasword protected and so need the password passed to the jar file
    The getStegoPassword requests the password from the user
    
    - Added getStegoPassword Property
    - Updated CLIInterface unit tests

--- src/components/cli-interface.test.ts
+++ src/components/cli-interface.test.ts
@@ -20,27 +20,44 @@ test('(Property) addCredential should return name and value', async () => {
 	expect(response.value).toBe('secret value')
 })
 
-test('(Property) addCredential should return name and value', async () => {
-	enquirer.prompt = jest
-		.fn()
-		.mockResolvedValueOnce({})
+test('(Property) editCredentials should return the edited credentials', async () => {
+	enquirer.prompt = jest.fn().mockResolvedValueOnce({})
 
 	const terminal = new CLIInterface()
-	await terminal.editCredentials({ secret: 'word', 'another Secret': 'wonderful' })
+	await terminal.editCredentials({
+		secret: 'word',
+		'another Secret': 'wonderful',
+	})
 
 	expect(enquirer.prompt).toHaveBeenCalledWith({
-		choices: [{
-			initial: "word",
-			message: "secret",
-			name: "secret",
-		},
-		{
-			initial: "wonderful",
-			message: "another Secret",
-			name: "anotherSecret",
-		},
-	],
-	message: "Current Credentials:",
-	name: "editedCredentials",
-	type: "form",})
+		choices: [
+			{
+				initial: 'word',
+				message: 'secret',
+				name: 'secret',
+			},
+			{
+				initial: 'wonderful',
+				message: 'another Secret',
+				name: 'anotherSecret',
+			},
+		],
+		message: 'Current Credentials:',
+		name: 'editedCredentials',
+		type: 'form',
+	})
+})
+
+test('(Property) getStegoPassword should call the prompt function', async () => {
+	enquirer.prompt = jest.fn().mockResolvedValueOnce({})
+
+	const terminal = new CLIInterface()
+	await terminal.getStegoPassword()
+
+	expect(enquirer.prompt).toHaveBeenCalledTimes(1)
+	expect(enquirer.prompt).toHaveBeenCalledWith({
+		message: 'Please enter the password for your stego image:',
+		name: 'stegoPassword',
+		type: 'password',
+	})
 })
