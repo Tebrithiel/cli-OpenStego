73c4f787a3724ecad1eced4cc48460528e180873
Tebrithiel <20450667+Tebrithiel@users.noreply.github.com>
2024-05-14 00:08:48 +1000

    Updated editCredentials to handle incorrect types
    The currentCredentials need to be a string so that the prompt form functions correctly
    There is now a check on each credential to ensure that they are of the type string
    
    - Added typeof check for mapped credentials that throws error when not string
    - Updated index to use new typings

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,18 @@ async function main() {
 	const openStego = new OpenStego(config)
 	const { stegoPassword } = await terminal.getStegoPassword()
 
-	const test1 = openStego.decryptStego({ stegoPassword })
-	console.log(typeof test1)
+	const decryptedStego = openStego.decryptStego({ stegoPassword }) as Record<
+		string,
+		string
+	>
+
+	console.log(decryptedStego)
 
 	const test2 = await terminal.addCredential()
 
 	console.log(test2)
 
-	const edited = await terminal.editCredentials(testCredentials)
+	const edited = await terminal.editCredentials(decryptedStego)
 
 	console.log(edited)
 }
